b8b0de4b93ef544c78edd745da2e82fcea797470
clemk <clemkfr@gmail.com>
2022-11-11 18:32:27 +0100

    first commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
-import { createApp } from 'vue'
 import App from './App.vue'
+import * as Vue from 'vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-createApp(App).use(store).use(router).mount('#app')
+
+
+
+
+const app = Vue.createApp(App)
+app.use(router)
+app.use(store)
+app.use(VueAxios, axios)
+app.mount("#app")
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,60 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
+
 
 export default createStore({
   state: {
+    products: [],
+    productsInBag: [],
   },
   mutations: {
+    loadProducts(state, products){
+            state.products = products;
+    },
+    addProd(state, product){
+           state.productsInBag.push(product);
+           console.log(product)
+           console.log(state.productsInBag)
+    },
+    removeFromBag(state, productId){
+      let updatedBag = state.productsInBag.filter(item => item.id != productId);
+       state.productsInBag = updatedBag 
+    }
+
+
+   
+    
   },
   actions: {
+    loadProducts( {commit} ){
+      axios
+      .get('https://fakestoreapi.com/products')
+      .then(response=>{
+        commit('loadProducts', response.data)
+      })
+
+    },
+    
+    addProd({commit}, product){
+      commit('addProd', product)
+    },
+    
+    removeFromBag({commit}, productId){
+    if(confirm('tu veux supprimer?')){
+      commit('removeFromBag', productId)
+       }
+    },
+
+    //action async d'appel à l'api dans app pour un acces global, 
+    //commit = appel d'une mutation ([1]=nom de la mutation, [2]parametre si ya )
+
+
+    // appler une action 'dispatch'
+    // appler une mutation 'commit' 
+
+
+    
+
   },
   modules: {
   }
